bbee6ee6c6ef7aed883cb0c0bb48054020468539
denysrodivilin <denysrodivilin@gmail.com>
2026-09-05 02:17:34 +0300

    Update styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kamenrbti.com.ua",
   integrations: [sitemap()],
+  fonts: [
+    {
+      provider: fontProviders.bunny(),
+      name: "Literata",
+      cssVariable: "--font-literata",
+      weights: [400, 700],
+      styles: ["normal", "italic"],
+      subsets: ["latin", "cyrillic", "cyrillic-ext"],
+    },
+  ],
 });
